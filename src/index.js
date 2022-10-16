5db7912a1abc719adbd02933dc0c5c04a8840ccc
SvitlanaStadnyk <svitstadnyk@gmail.com>
2022-10-16 22:35:15 +0300

    commit 221016

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import './css/styles.css';
+import './css/common.css';
 import { Notify } from 'notiflix';
-// const axios = require('axios').default;
 import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -9,16 +8,12 @@ let searchQueryResult = '';
 let q = '';
 let pageN = 1;
 let gallery = new SimpleLightbox('.gallery a', {
-  /* options */ enableKeyboard: true,
+  enableKeyboard: true,
 });
-
-//Objects
-
-//pixabayObj
 
 const pixabayAPI = {
   baseUrl: 'https://pixabay.com/api/',
-  key: '30621477-9ef591ebb3357a3c459a51ae5',
+  key: '3705719-850a353db1ffe60c326d386e6',
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: 'true',
@@ -27,72 +22,49 @@ const pixabayAPI = {
   per_page: '40',
 };
 
-//markup
-
 const markupData = {
   markup: '',
   htmlCode: '',
 };
 
-// -------------------------------------------------------------------------------------------------------
-// searchForm and gallery find in DOM
-
 const searchForm = document.querySelector('.search-form');
 const gallerySelector = document.querySelector('.gallery');
 
-// -------------------------------------------------------------------------------------------------------
-// event listener search form
-
 searchForm.addEventListener('submit', async e => {
   e.preventDefault();
-
   const {
     elements: { searchQuery },
   } = e.target;
-
   searchQueryResult = searchQuery.value;
-
-  // console.log
   console.log('searchQueryResult:', `"${searchQueryResult}"`);
   console.log('q:', `"${q}"`);
-
   if (searchQueryResult === '') {
     console.log(searchQueryResult);
     gallerySelector.innerHTML = '';
     btnLoadMore.classList.remove('is-visible');
-
     return Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
   }
-
   if (searchQueryResult !== q) {
     console.log('CHANGED!!! NOT EMPTY QUERY');
-
     pageN = 1;
     pixabayAPI.page = `${pageN}`;
-
     gallerySelector.innerHTML = '';
     btnLoadMore.classList.remove('is-visible');
   } else {
     console.log('page+1!!!');
-
     pageN += 1;
     pixabayAPI.page = `${pageN}`;
-
     btnLoadMore.classList.remove('is-visible');
   }
-
   q = searchQueryResult;
 
   try {
     const results = await fetchPhotos(searchQueryResult);
     markupData.htmlCode = await renderedPhotos(results);
-
     gallerySelector.insertAdjacentHTML('beforeend', markupData.htmlCode);
     btnLoadMore.classList.add('is-visible');
-
-    // simpleLightbox gallery destroys and reinitilized
     gallery.refresh();
 
     const {
@@ -113,35 +85,23 @@ searchForm.addEventListener('submit', async e => {
     }
 
     Notify.success(`'Hooray! We found ${results.totalHits} images.'`);
-
-    //console.log
     console.log('results', results);
   } catch (error) {
     Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
   }
-
-  // console.log
   console.log('');
 });
-
-// -------------------------------------------------------------------------------------------------------
-// button load more
-
 const btnLoadMore = document.querySelector('.load-more');
 btnLoadMore.addEventListener('click', async () => {
   pageN += 1;
   pixabayAPI.page = `${pageN}`;
-
   try {
     const results = await fetchPhotos(searchQueryResult);
     markupData.htmlCode = await renderedPhotos(results);
-
     gallerySelector.insertAdjacentHTML('beforeend', markupData.htmlCode);
     btnLoadMore.classList.add('is-visible');
-
-    // simpleLightbox gallery destroys and reinitilized
     gallery.refresh();
 
     const {
@@ -167,13 +127,9 @@ btnLoadMore.addEventListener('click', async () => {
       "We're sorry, but you've reached the end of search results."
     );
   }
-
   console.log('btnLoadMore working');
   console.log('');
 });
-
-// -------------------------------------------------------------------------------------------------------
-// fetch photos function
 
 async function fetchPhotos(searchQueryResult) {
   const {
@@ -186,13 +142,9 @@ async function fetchPhotos(searchQueryResult) {
     page,
     per_page,
   } = pixabayAPI;
-
   pixabayAPI.page = `${pageN}`;
 
   console.log('page', page);
-
-  // const response = await fetch(`${baseUrl}?key=${key}&q=${q}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}&order=${order}&page=${page}&per_page=${per_page}`);
-  // const results = await response.json();
 
   const response = await axios.get(
     `${baseUrl}?key=${key}&q=${q}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}&order=${order}&page=${page}&per_page=${per_page}`
@@ -203,16 +155,12 @@ async function fetchPhotos(searchQueryResult) {
   console.log('response', response);
   console.log('page', page);
 
-  //results destruction
-
   const { total, totalHits, hits } = results;
   const totalPages = Math.ceil(totalHits / per_page);
 
   if (total === 0) {
     throw new Error();
   }
-
-  //total pages check
   if (page >= totalPages) {
     btnLoadMore.classList.remove('is-visible');
     Notify.failure(
@@ -223,19 +171,11 @@ async function fetchPhotos(searchQueryResult) {
 
   console.log('totalHits', totalHits);
   console.log('per_page', per_page);
-
   console.log('totalPages=', totalPages);
-
-  //received data
   return results;
 }
-
-// -------------------------------------------------------------------------------------------------------
-// render photos function, make html markup
-
 async function renderedPhotos(results) {
   const { hits } = results;
-
   markupData.markup = hits
     .map(
       hit =>
@@ -259,6 +199,5 @@ async function renderedPhotos(results) {
 </div></a>`
     )
     .join('');
-
   return markupData.markup;
 }
